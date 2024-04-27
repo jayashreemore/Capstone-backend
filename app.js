b4001f4f465a9fd0c6a4588808c1387c4ec97f6e
Jayshree Morey <120532991+jayashreemore@users.noreply.github.com>
2024-04-26 23:34:13 -0400

    Server is running on port 5050

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+var cors = require('cors');
+var cookieParser = require('cookie-parser');
+
+//MIDDLEWARE
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true
+}));
+app.use(cookieParser());
+app.use(cors());
+
+// //port
+const port = process.env.PORT || 5050
+
+app.listen(port, () => {
+    console.log(` Server is running on port ${port}`);
+})
